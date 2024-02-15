66cd2201ef684c454dd86846ad5d3b80dad3ceb3
Julius John <julius.john@elev.ga.ntig.se>
2024-02-15 18:34:28 +0100

    Start Commit

---
+++ Slutp/Program.cs
@@ -0,0 +1,16 @@
+﻿global using Raylib_cs;
+
+Raylib.InitWindow(1200,1000, "TankTrouble");
+
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+
+
+    Raylib.BeginDrawing();
+
+
+
+    Raylib.EndDrawing();
+}
