977fe2170ede5ab7f3b0fc71c31385643d56d663
Julius John <julius.john@elev.ga.ntig.se>
2024-02-22 11:01:37 +0100

    player stamina method added + pseudo code

--- Slutp/Player.cs
+++ Slutp/Player.cs
@@ -2,16 +2,39 @@ class Player
 {
     public Rectangle Hitbox = new(360, 550, 80, 20);
     public int Level = 1;
+      public int ExpMax = 100;
+    public int Exp = 0;
     public int Hp = 200;
     public int HitPointMax = 200;
-    public int ExpMax = 100;
-    public int Exp = 0;
-    public float speed = 5;
-    public int stamina = 50;   
-
+    public float Speed = 5;
+    public int Stamina = 100;
+    public int StaminaMax = 100;   
+    public int Hunger = 100;
+    public int HungerMax = 100;
     
     public void Update()
     {
-        if (Raylib.IsKeyDown(KeyboardKey.A)) Hitbox.X -= speed;
+        if (Raylib.IsKeyDown(KeyboardKey.A)) Hitbox.X -= Speed;
+    }
+
+    public void Action()
+    {
+        for (int Stamina = 0; Stamina < StaminaMax; Stamina++)
+        {
+            
+        }
     }
 }
+
+/* public method (stamina)
+for each second stamina < staminaMax
+stamina += 1
+*/
+
+
+/*public method (action):
+for every action :
+-5 stamina
+
+*/
+
